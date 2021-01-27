a569ed05df0a6036a5d47e9f6673469fd77b1e40
Fomagran <fomagran6@naver.com>
2021-01-27 18:58:22 +0900

    xib로 뷰 만든거 코드로 세팅하고 뷰컨트롤러로 연결 (기본 세팅완료)

--- Swift Weather/View/WeatherView.swift
+++ Swift Weather/View/WeatherView.swift
@@ -7,23 +7,46 @@
 
 import UIKit
 
-class WeatherView: UIViewController {
+class WeatherView: UIView {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var weatherInfoLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var bottomContainer: UIView!
+    @IBOutlet weak var hourlyWeatherCollectionView: UICollectionView!
+    @IBOutlet weak var infoCollectionView: UICollectionView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override init(frame:CGRect) {
+        super.init(frame: frame)
+        mainInit()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    private func mainInit() {
+        Bundle.main.loadNibNamed("WeatherView", owner: self, options: nil)
+        addSubview(mainView)
+        mainView.frame = self.bounds
+        mainView.autoresizingMask = [.flexibleHeight,.flexibleWidth]
+        setUpTableView()
+        sepUpHourlyCollectionView()
+        setUpInfoCollectionVIew()
+    }
+    
+    private func setUpTableView() {
+        
+    }
+    private func sepUpHourlyCollectionView() {
+        
+    }
+    private func setUpInfoCollectionVIew() {
+        
+    }
+    
 }
--- Swift Weather/ViewController/WeatherViewController.swift
+++ Swift Weather/ViewController/WeatherViewController.swift
@@ -8,14 +8,17 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+    
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    
-
+        
+        let weatherView = WeatherView()
+        scrollView.addSubview(weatherView)
+        weatherView.frame = CGRect(x: 0, y: 0, width:scrollView.bounds.width, height: scrollView.bounds.height)
+        
+        
     }
 }
 
