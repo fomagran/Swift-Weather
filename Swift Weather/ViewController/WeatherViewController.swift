f4ffa1f4c5954481906f70b72118dc527cbe4a0b
Fomagran <fomagran6@naver.com>
2021-02-09 12:54:32 +0900

    Hourly 날씨정보 Weekly날씨정보 띄우기

--- Swift Weather/ViewController/WeatherViewController.swift
+++ Swift Weather/ViewController/WeatherViewController.swift
@@ -16,21 +16,43 @@ class WeatherViewController: UIViewController {
         
         
         
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
-  
+        
+        let weatherView = WeatherView()
+        weatherView.frame = CGRect(x: 0, y: 0, width: scrollView.bounds.width, height: scrollView.bounds.height)
+        scrollView.addSubview(weatherView)
+        getCurrentWeather(weatherView: weatherView)
+        getWeeklyWeahter(weatherView: weatherView)
+        getHourlyWeather(weatherView: weatherView)
     }
     
     
     //MARK:Download Weather
-    private func getCurrentWeather() {
+    private func getCurrentWeather(weatherView:WeatherView) {
+        
+        weatherView.currentWeather = CurrentWeather()
+        weatherView.currentWeather.getCurrentWeather { (success) in
+        weatherView.refreshData()
+            
+        }
+    }
+    private func getWeeklyWeahter(weatherView:WeatherView) {
+        
+        WeeklyForecast.downloadWeeklyForecastWeather { (weahterForecasts) in
+            weatherView.weeklyWeahterForecastData = weahterForecasts
+            weatherView.tableView.reloadData()
+        }
         
     }
-    private func getWeeklyWeahter() {
+    private func getHourlyWeather(weatherView:WeatherView) {
         
-    }
-    private func getHourlyWeather() {
+        HourlyForecast.downloadHourlyForecastWeather { (weatherForecasts) in
+            weatherView.dailyWeatherForecastData = weatherForecasts
+            weatherView.hourlyWeatherCollectionView.reloadData()
+        }
         
     }
 }
